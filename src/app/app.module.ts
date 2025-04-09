4bc8ccfcc0920f03305c23f0ff6bba88500dcc16
fatoma <fatomandaou5@gmail.com>
2025-04-09 23:40:47 +0000

    Affichage des cours

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseComponent } from './core/course/course.component';
+import { ListCourseComponent } from './core/course/list-course/list-course.component';
+import { ViewerCourseComponent } from './core/course/viewer-course/viewer-course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent,
+    ListCourseComponent,
+    ViewerCourseComponent
   ],
   imports: [
     BrowserModule,
